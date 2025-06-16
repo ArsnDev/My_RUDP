48dfb50bfcb0bbee6b35bbe5da3df0a15d4c7b90
YongminGwon <clumsyrhymer@gmail.com>
2025-06-16 11:20:44 +0900

    sync ack implemented

--- DummyClient/main.cpp
+++ DummyClient/main.cpp
@@ -11,13 +11,24 @@ int main()
 	if (iResult != NO_ERROR)
 	{
 		std::cout << "Error at WSAStartup()\n";
+		return 1;
 	}
-	SOCKET sendSocket = INVALID_SOCKET;
-\
-	sendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sendSocket == INVALID_SOCKET)
+
+	SOCKET clientSocket = INVALID_SOCKET;
+	clientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (clientSocket == INVALID_SOCKET)
 	{
 		std::cout << "socket function failed with error : " << WSAGetLastError() << "\n";
+		WSACleanup();
+		return 1;
+	}
+
+	unsigned long nonBlocking = 1;
+	iResult = ioctlsocket(clientSocket, FIONBIO, &nonBlocking);
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "ioctlsocket failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(clientSocket);
 		WSACleanup();
 		return 1;
 	}
@@ -25,10 +36,11 @@ int main()
 	sockaddr_in serverAddr;
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(9000);
-	
 	iResult = inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 
 	static unsigned int mySequenceNum = 0;
+	char recvBuf[sizeof(MyPacket)];
+	int bufLen = sizeof(MyPacket);
 
 	std::cout << "Starting dummy client...\n";
 
@@ -38,17 +50,37 @@ int main()
 		packetToSend.header.sequenceNum = mySequenceNum++;
 		packetToSend.header.ackNum = 0;
 
-		iResult = sendto(sendSocket, reinterpret_cast<char*>(&packetToSend), sizeof(packetToSend), 0, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr));
+		iResult = sendto(clientSocket, reinterpret_cast<char*>(&packetToSend), sizeof(packetToSend), 0, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr));
 		if (iResult == SOCKET_ERROR)
 		{
 			std::cout << "sendto failed with error: " << WSAGetLastError() << "\n";
-			closesocket(sendSocket);
+			closesocket(clientSocket);
 			WSACleanup();
 			return 1;
 		}
-		std::cout << "Packet Sent! Seq: " << packetToSend.header.sequenceNum - 1 << "\n";
+		std::cout << "Packet Sent! Seq: " << packetToSend.header.sequenceNum << "\n";
+		
+		iResult = recvfrom(clientSocket, recvBuf, bufLen, 0, nullptr, nullptr);
+		if (iResult > 0)
+		{
+			RUDPHeader* ackHeader = reinterpret_cast<RUDPHeader*>(recvBuf);
+			std::cout << "  -> ACK Received for my Seq: " << ackHeader->ackNum
+				<< " (Server Seq: " << ackHeader->sequenceNum << ")\n";
+		}
+		else if (iResult == SOCKET_ERROR)
+		{
+			int errorCode = WSAGetLastError();
+			if (errorCode == WSAEWOULDBLOCK)
+			{
+			}
+			else
+			{
+				std::cout << "recvfrom failed with error: " << errorCode << "\n";
+			}
+		}
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
+	closesocket(clientSocket);
 	WSACleanup();
 	return 0;
 }
--- RUDP/main.cpp
+++ RUDP/main.cpp
@@ -32,7 +32,13 @@ int main()
 
 	recvAddr.sin_family = AF_INET;
 	recvAddr.sin_port = htons(9000);
-
+	
+	char reuseAddr = 1;
+	iResult = setsockopt(recvSocket, SOL_SOCKET, SO_REUSEADDR, &reuseAddr, sizeof(reuseAddr));
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "setsockopt for SO_REUSEADDR failed with error: " << WSAGetLastError() << "\n";
+	}
 	iResult = inet_pton(AF_INET, "127.0.0.1", &recvAddr.sin_addr);
 	iResult = bind(recvSocket, reinterpret_cast<SOCKADDR*>(&recvAddr), sizeof(recvAddr));
 	if (iResult == SOCKET_ERROR)
@@ -47,6 +53,8 @@ int main()
 		std::cout << "bind successed\n";
 	}
 
+	static unsigned int serverSequenceNum = 0;
+
 	while (true)
 	{
 		iResult = recvfrom(recvSocket, recvBuf, bufLen, 0, reinterpret_cast<SOCKADDR*>(&senderAddr), &senderAddrSize);
@@ -55,8 +63,16 @@ int main()
 			std::cout << "recvfrom failed " << WSAGetLastError() << "\n";
 		}
 
-		RUDPHeader* header = reinterpret_cast<RUDPHeader*>(recvBuf);
-		std::cout << "Packet Recved Seq: " << header->sequenceNum << ", Ack: " << header->ackNum << "\n";
+		RUDPHeader* recvHeader = reinterpret_cast<RUDPHeader*>(recvBuf);
+		std::cout << "Packet Recved Seq: " << recvHeader->sequenceNum << ", Ack: " << recvHeader->ackNum << "\n";
+		unsigned int seqToAck = recvHeader->sequenceNum;
+
+		RUDPHeader sendHeader;
+		sendHeader.sequenceNum = serverSequenceNum++;
+		sendHeader.ackNum = seqToAck;
+
+		sendto(recvSocket, reinterpret_cast<char*>(&sendHeader), sizeof(sendHeader), 0, reinterpret_cast<SOCKADDR*>(&senderAddr), sizeof(senderAddr));
+		std::cout << " -> Sent ACK for Seq: " << seqToAck << "\n";
 	}
 
 	WSACleanup();
